631fc87d42846fc87a6228b251e69a723b82b400
Lizzie Siegle <esiegle@brynmawr.edu>
2020-05-27 12:11:45 -0700

    this is it

--- assets/index.js
+++ assets/index.js
@@ -1,47 +1,50 @@
 (() => {
-  "use strict";
+  'use strict';
   const TWILIO_DOMAIN = location.host;
-  const ROOM_NAME = "tf";
+  const ROOM_NAME = 'tf';
   const Video = Twilio.Video;
   let videoRoom, localStream;
   const video = document.getElementById("video");
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
   const minConfidence = 0.2;
-  const VIDEO_WIDTH = document.getElementById("video").offsetWidth;
-  const VIDEO_HEIGHT = document.getElementById("video").offsetHeight;
+  const VIDEO_WIDTH = 320;
+  const VIDEO_HEIGHT = 240;
   const frameRate = 20;
 
   // preview screen
-  navigator.mediaDevices
-    .getUserMedia({ video: true, audio: true })
-    .then((vid) => {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+    .then(vid => {
       video.srcObject = vid;
       localStream = vid;
       const intervalID = setInterval(async () => {
         try {
           // ctx.scale(-1, 1);
           // ctx.translate(-VIDEO_WIDTH, 0);
-          canvas.width = video.offsetWidth;
-          canvas.height = video.offsetHeight;
-          ctx.restore();
+          // ctx.restore();
           estimateMultiplePoses();
         } catch (err) {
-          clearInterval(intervalID);
-          setErrorMessage(err.message);
+          clearInterval(intervalID)
+          setErrorMessage(err.message)
         }
-      }, Math.round(1000 / frameRate));
-      return () => clearInterval(intervalID);
+      }, Math.round(1000 / frameRate))
+      return () => clearInterval(intervalID)
     });
   function drawKeypoints(keypoints) {
     for (let i = 0; i < keypoints.length; i++) {
       const keypoint = keypoints[i];
       console.log(`keypoint in drawkeypoints ${keypoint}`);
       const { y, x } = keypoint.position;
+      //webcamKeypoints.push(keypoint);
       drawPoint(y, x, 3);
     }
   }
-  function drawSegment(pair1, pair2, color, scale) {
+  function drawSegment(
+    pair1,
+    pair2,
+    color,
+    scale
+  ) {
     ctx.beginPath();
     ctx.moveTo(pair1.x * scale, pair1.y * scale);
     ctx.lineTo(pair2.x * scale, pair2.y * scale);
@@ -58,7 +61,12 @@
     );
 
     adjacentKeyPoints.forEach((keypoint) => {
-      drawSegment(keypoint[0].position, keypoint[1].position, color, 1);
+      drawSegment(
+        keypoint[0].position,
+        keypoint[1].position,
+        color,
+        1,
+      );
     });
   }
   function drawPoint(y, x, r) {
@@ -71,15 +79,14 @@
   const estimateMultiplePoses = () => {
     posenet
       .load()
-      .then(function(net) {
+      .then(function (net) {
         console.log("estimateMultiplePoses .... ");
         return net.estimatePoses(video, {
           decodingMethod: "single-person",
         });
       })
-      .then(function(poses) {
+      .then(function (poses) {
         console.log(`got Poses ${JSON.stringify(poses)}`);
-        
         canvas.width = VIDEO_WIDTH;
         canvas.height = VIDEO_HEIGHT;
         ctx.clearRect(0, 0, VIDEO_WIDTH, VIDEO_HEIGHT);
@@ -127,39 +134,60 @@
     console.log(`Disconnected from Room ${videoRoom.name}`);
     joinRoomButton.disabled = false;
     leaveRoomButton.disabled = true;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    //leave room, clear canvas
-    document.removeChild(canvas);
   };
 })();
 
 const participantConnected = (participant) => {
   console.log(`Participant ${participant.identity} connected'`);
 
-  const div = document.createElement("div");
+  const div = document.createElement('div');
   div.id = participant.sid;
 
-  participant.on("trackSubscribed", (track) => trackSubscribed(div, track));
-  participant.on("trackUnsubscribed", trackUnsubscribed);
+  participant.on('trackSubscribed', track => trackSubscribed(div, track));
+  participant.on('trackUnsubscribed', trackUnsubscribed);
 
-  participant.tracks.forEach((publication) => {
+  participant.tracks.forEach(publication => {
     if (publication.isSubscribed) {
       trackSubscribed(div, publication.track);
     }
   });
   document.body.appendChild(div);
-};
+  //new div
+}
 
 const participantDisconnected = (participant) => {
   console.log(`Participant ${participant.identity} disconnected.`);
   document.getElementById(participant.sid).remove();
-};
+}
 
 const trackSubscribed = (div, track) => {
   div.appendChild(track.attach());
-};
+}
 
 const trackUnsubscribed = (track) => {
-  track.detach().forEach((element) => element.remove());
-};
+  track.detach().forEach(element => element.remove());
+}
+function estimatePoses(capture) {
+  // call posenet to estimate a pose
+  net
+    .estimateMultiplePoses(capture.elt, imageScaleFactor, flipHorizontal, 16, 1)
+    .then(function (poses) {
+      if (poses.length > 0) {
+        const pose = poses[0];
+
+        if (pose.score > 0.1) {
+          const leftHand = pose.keypoints[posenet.partIds["leftWrist"]];
+          if (leftHand.score > 0.1) {
+            leftHandX = leftHand.position.x;
+            leftHandY = leftHand.position.y;
+          }
+
+          const rightHand = pose.keypoints[posenet.partIds["rightWrist"]];
+          if (rightHand.score > 0.1) {
+            rightHandX = rightHand.position.x;
+            rightHandY = rightHand.position.y;
+          }
+        }
+      }
+    });
+}
